5c73bbf877f97fc718e7e5058696a5b386bbf5e0
Amirsina A. Albekord <aalbekor@uci.edu>
2025-10-27 16:18:51 -0700

    list component functionality

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,32 @@
-import "./List.css"
+import "./List.css";
 
 const List = ({ data }) => {
-    // NTS: when mapping, make sure to have a key attribute
     return (
-        <> 
-            <h1>List</h1>
-            {/* learn to map a table */}
+        <>
+            <table className="data-table">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Temperature</th>
+                        <th>Wind</th>
+                        <th>Snow</th>
+                        <th>Max UV</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item["datetime"]}</td>
+                            <td>{item["temp"]}</td>
+                            <td>{item["wind_spd"]}</td>
+                            <td>{item["snow"]}</td>
+                            <td>{item["max_uv"]}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
-    )
-}
+    );
+};
+
 export default List;
